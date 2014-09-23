dfecb8e4a26fe7c08a601406dd82b2d3d29ec4b4
Andrew Minion <andrew@andrewrminion.com>
2014-09-22 21:03:23 -0400

    swap webfonts

--- functions.php
+++ functions.php
@@ -3,8 +3,11 @@
 include('functions-branding.php');
 include('functions-church.php');
 
-/* deregister Bitter and Open Sans webfonts */
+/* deregister Bitter and Open Sans webfonts, replace with EB Garamond and Open Sans */
 function deregister_default_fonts() {
     wp_deregister_style( 'twentythirteen-fonts' );
+    wp_register_style( 'lbc-webfonts', '//fonts.googleapis.com/css?family=EB+Garamond' );
+    wp_enqueue_style( 'lbc-webfonts' );
 }
 add_action( 'wp_enqueue_scripts', 'deregister_default_fonts', 100 );
+
