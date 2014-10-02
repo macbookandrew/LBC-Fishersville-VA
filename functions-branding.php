13245486a28b4762b9329a7a70272479ce24780e
Andrew Minion <andrew@andrewrminion.com>
2014-10-02 08:59:36 -0400

    update release date

--- functions-branding.php
+++ functions-branding.php
@@ -37,7 +37,7 @@
 	function armd_dashboard_widget_function() {
 		// Entering the text between the quotes
 		echo "<ul>
-		<li>Release Date: February 2013</li>
+		<li>Release Date: October 2014</li>
 		<li>Developer: <a href=\"http://andrewrminion.com/\" target=\"_blank\">AndrewRMinion Design</a></li>
 		<li>Hosting provider: ANHosting (AndrewRMinion Design)</li>
 		</ul>";
